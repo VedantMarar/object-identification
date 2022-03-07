749a0c15011c7b467e21b146b5ce1809183a1bef
VedantMarar <72292303+VedantMarar@users.noreply.github.com>
2022-03-07 18:18:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 img = "";
+status = "";
 
 function setup(){
     canvas = createCanvas(640,420);
     canvas.center();
-
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "status: Detecting objects"
 }
 
 function preload(){
@@ -17,4 +19,24 @@ function draw(){
     noFill();
     rect(30,60,450,350);
     text("dog",45,75);
+    fill("yellow")
+    noFill();
+    text("cat",320,120)
+    stroke("yellow");
+    rect(300,90,270,320);
+}
+
+function modelLoaded(){
+    console.log("Model loaded!");
+    status = true;
+    objectDetector.detect(img,gotResult);
+}
+
+function gotResult(error,results){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+    }
 }
